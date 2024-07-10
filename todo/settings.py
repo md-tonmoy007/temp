f321e8c41a8840d2364f22d7df022a252e1b106e
IamMrRobot <iammrrobot@MrRobot.MrRobot>
2024-07-10 20:31:34 +0600

    added something

--- todo/settings.py
+++ todo/settings.py
@@ -122,3 +122,11 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STATIC_URL = 'static/'
+
+#Add this in your settings.py file:
+STATICFILES_DIRS = [
+    BASE_DIR / "mystaticfiles"
+]
+
